b73ce4350f57e26463bb885621fcbce7a8c85aca
jonas de brito balero <72454186+jonasbb28@users.noreply.github.com>
2022-07-22 10:50:42 -0300

    resolução do exercício16

--- aula14ex/ex016/script.js
+++ aula14ex/ex016/script.js
@@ -1,17 +1,34 @@
-function verificar() {
+function contar() {
     var numini = window.document.getElementById("numini")
     var numfim = window.document.getElementById("numfim")
     var numpas = window.document.getElementById("numpas")
     var cont = window.document.getElementById("cont")
-    if (numini.value.length == 0 || numfim.value.length == 0 ) {
+    if (numini.value.length == 0 || numfim.value.length == 0 || numfim.value.length == 0) {
         cont.innerHTML = "Impossível contar!"
-    } else if (numpas.value == 0) {
-        window.alert("Passo inválido! Considerando PASSO 1")
-        numpas = 1
+        // window.alert("[ERRO] Faltam dados!")
     }
     else {
-        for (var c = numini; c <= numfim; c += numpas) {
-            cont.innerHTML = c.value
+        cont.innerHTML = "Contando: <br>"
+        let i = Number(numini.value)
+        let f = Number(numfim.value)
+        let p = Number(numpas.value)
+        if (numpas.value == 0) {
+            window.alert("Passo inválido! Considerando PASSO 1")
+            p = 1
         }
+        if (i < f) {
+            // Contagem crescente
+            for (let c = i; c <= f; c += p) {
+                cont.innerHTML += ` ${c} \u{1F449}`
+            }
+            cont.innerHTML += `\u{1F3C1}`
+        } else {
+            // Contagem regressiva
+            for (let c = i; c >= f; c -= p){
+                cont.innerHTML += ` ${c} \u{1F449}`
+            }
+            cont.innerHTML += `\u{1F3C1}`
+        }
+
     }
 }

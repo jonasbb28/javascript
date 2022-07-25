786a14c35f29361b940689dce77ebff6cdb442a6
jonas de brito balero <72454186+jonasbb28@users.noreply.github.com>
2022-07-25 10:17:37 -0300

    realizei o exercício 17

--- aula14ex/ex017/script.js
+++ aula14ex/ex017/script.js
@@ -1,1 +1,18 @@
-alert("Olá!")
+function tabuada() {
+    var num = window.document.getElementById("txtn")
+    var tab = window.document.getElementById("seltab")
+    if (num.value.length == 0) {
+        window.alert("Por favor, dígite um número!")
+    } else {
+        var n = Number(num.value)
+        var c = 1
+        tab.innerHTML = ""
+        while (c <= 10) {
+            let item = document.createElement("option")
+            item.text = `${n} x ${c} = ${n*c}`
+            item.value = `tab${c}`
+            tab.appendChild(item)
+            c++
+        }
+    }
+}
